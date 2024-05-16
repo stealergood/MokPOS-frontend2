e0da80a3cbe9544c54916b178ae5095e1f56ee66
Denny Edy Saputra <ncstealer@gmail.com>
2024-05-16 22:27:19 +0700

    modified:   App.js 	new file:   src/screen/startScreen.jsx

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 
 export default function App() {
   return (
-    <View className="w-full h-28 bg-slate-400 flex justify-center items-center">
-      <Text className="text-lg">Open up App.js to start working on !</Text>
+    <View className="pt-8 w-full h-full">
+      <View className="w-full h-10 bg-slate-200 flex flex-row">
+        <View className=" bg-slate-300 w-1/4 flex justify-center items-center">
+          <Text>Logo</Text>
+        </View>
+        <View className=" bg-gray-400 w-full flex justify-center items-center">
+          <Text>Login</Text>
+        </View>
+      </View>
+
+      <View className="w-full h-full bg-orange-600">
+        <View className="">
+          <Text>Email</Text>
+          <TextInput></TextInput>
+        </View>
+        <View></View>
+        <View></View>
+      </View>
+      
     </View>
   );
 }
