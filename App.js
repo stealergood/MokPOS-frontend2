925441c7e719865a4533b34ede8f2eeb75f94b2d
Denny Edy Saputra <ncstealer@gmail.com>
2024-05-16 20:13:52 +0700

    chore: Update App.js and babel.config.js

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="w-full h-28 bg-slate-400 flex justify-center items-center">
+      <Text className="text-lg">Open up App.js to start working on !</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
